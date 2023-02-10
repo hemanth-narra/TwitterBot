41b892f612401838b2fd46e4840695646dcdbc21
justforvpn <tailscalevpnnetwork@gmail.com>
2023-02-10 21:41:46 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Telegram Bot for Downloading Twitter Videos
+
+A simple telegram bot that can download videos from Twitter and send them to you.
+
+## Getting Started
+
+These instructions will get you a copy of the bot up and running on your Ubuntu machine for development and testing purposes.
+
+### Prerequisites
+
+- A telegram account and bot token
+- Python 3.10 installed on your system
+
+## Deployment on Ubuntu VPS
+
+These steps will guide you to deploy the bot on an Ubuntu VPS
+
+1. Update the system
+`sudo apt-get update`
+
+2. Install python and pip
+`sudo apt-get install python3 python3-pip`
+### Installing
+
+1. Clone the repository
+
+
+
+
+
+## Built With
+
+- [Pyrogram](https://docs.pyrogram.org/intro/quickstart) - The MTProto API framework for Telegram
+- [youtube-dl](https://ytdl-org.github.io/youtube-dl/index.html) - A command-line program to download videos from YouTube.com and a few more sites
+
+## Contributing
+
+Feel free to create a pull request or issue if you have any suggestions or bugs to report.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+
+
---
+++ bot.py
@@ -0,0 +1,40 @@
+import config
+import youtube_dl
+from pyrogram import Client, filters
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Get api_id and api_hash from https://my.telegram.org
+api_id = int(config.API_ID)
+api_hash = config.API_HASH
+
+app = Client('my_bot', api_id, api_hash)
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("Hello! I am a bot that can download videos from Twitter and send them to you.")
+
+@app.on_message(filters.text & filters.regex(r"^https://twitter.com.*"))
+async def handle_twitter_url(app, message):
+    try:
+        logging.info(f'got the url: {message.text}')
+        tweet_url = message.text
+        ydl_opts = {
+            'outtmpl': 'video.%(ext)s',
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            logging.info("Downloading video using youtube-dl")
+            ydl.download([tweet_url])
+        logging.info("Sending video to the user")
+        # Send the video to the user
+        await message.reply_video("video.mp4")
+        logging.info("Deleting video from disk")
+        # Delete the video from the computer
+        os.remove("video.mp4")
+    except Exception as e:
+        logging.error(e)
+        await message.reply_text(f'Unable to find the video for {message.text}')
+
+app.run()
